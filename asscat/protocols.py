3e0cc79c7a0aa523f6ec182c13a5d936a13dd8d7
DCx7C5 <dcxdevelopment@protonmail.com>
2023-10-24 23:57:09 +0200

    Changed to StreamReaderProtocol / Fix upgrade shell to fully compatible pty

--- asscat/protocols.py
+++ asscat/protocols.py
@@ -1,44 +1,69 @@
+import itertools
 from asyncio import (
     AbstractEventLoop,
-    Transport,
     StreamReader,
-    StreamWriter, Protocol
+    StreamWriter,
+    StreamReaderProtocol, Task,
 )
-from asyncio.trsock import TransportSocket
-from typing import Optional, Union
-
-from asscat.sessions import Session
+from asyncio.streams import FlowControlMixin # noqa
+from typing import Optional
 
 
-class RevshellProtocol(Protocol):
+class BaseRevShellProtocol(StreamReaderProtocol): ...
+
+
+class RevShellSession(BaseRevShellProtocol):
+    conn_counter = itertools.count()
 
     def __init__(self, loop: AbstractEventLoop, manager):
         self.loop = loop
+        super().__init__(
+            stream_reader=StreamReader(loop=self.loop),
+            client_connected_cb=self._connection_made_cb,
+            loop=self.loop,
+        )
         self.manager = manager
-        self.transport: Optional[Union[Transport, TransportSocket]] = None
-        self.reader = StreamReader(loop=loop)
-        self.session: Optional[Session] = None
-        self.stdout_writer: StreamWriter = manager.stdout_writer
-        self.is_connected = False
+        self._reader: Optional[StreamReader] = None
+        self._writer: Optional[StreamWriter] = None
+        self.transport = None
+        self.connection_closed: bool = True
+        self._connection_closed: bool = True
+        self._conn_handler_task: Optional[Task] = None
+        self._data_handler_task: Optional[Task] = None
+        self._send_cmd_task: Optional[Task] = None
 
     def connection_made(self, transport):
-        self.is_connected = True
+        super().connection_made(transport)
         self.transport = transport
-        sessions_count = len(self.manager.sessions)
-        session = Session(
-            loop=self.loop,
-            protocol=self,
-            session_id=sessions_count,
-        )
-        self.manager.sessions.append(session)
-        self.session = session
-        print(f'Connection made from {transport.get_extra_info("peername")}')
-        self.loop.create_task(self.session.send_cmd())
+        self.manager.sessions.append(self)
+        self.connection_closed = False
+        print(f'connection made from {transport.get_extra_info("peername")}')
+        self._send_cmd_task = self.loop.create_task(self._send_cmd())
 
     def data_received(self, data: bytes):
-        self.reader.feed_data(data)
-        self.stdout_writer.transport.write(data)
-        self.loop.create_task(self.session.send_cmd())
+        super().data_received(data)
+        self._data_handler_task = self.loop.create_task(self._handle_data())
+        if self._send_cmd_task.done():
+            self._send_cmd_task = self.loop.create_task(self._send_cmd())
 
     def connection_lost(self, exc):
-        self.is_connected = False
+        super().connection_lost(exc)
+        self.connection_closed = self._connection_closed = True
+        try:
+            self.manager.sessions.remove(self)
+        except ValueError:
+            pass
+
+    async def _handle_data(self):
+        data = await self._reader.read(8192)
+        self.manager.stdout_writer.write(data)
+        await self.manager.stdout_writer.drain()
+
+    async def _send_cmd(self):
+        cmd = await self.manager.stdin_reader.readline()
+        self._writer.write(cmd)
+        await self._writer.drain()
+
+    def _connection_made_cb(self, reader, writer):
+        self._reader = self.reader = reader
+        self._writer = self.writer = writer
