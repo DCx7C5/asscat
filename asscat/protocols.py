2ce995aec2b3d28d46fefdc2abaeca1b7efa4d3f
DCx7C5 <dcxdevelopment@protonmail.com>
2023-11-02 23:50:55 +0100

    Protocols are now sessions

--- asscat/protocols.py
+++ asscat/protocols.py
@@ -2,11 +2,14 @@ import itertools
 from asyncio import (
     AbstractEventLoop,
     StreamReader,
-    StreamWriter,
-    StreamReaderProtocol, Task,
+    StreamReaderProtocol,
+    Task,
+    SubprocessProtocol,
 )
-from asyncio.streams import FlowControlMixin # noqa
+from asyncio.streams import FlowControlMixin  # noqa
 from typing import Optional
+
+from asscat.streams import TCPStream
 
 
 class BaseRevShellProtocol(StreamReaderProtocol): ...
@@ -16,37 +19,35 @@ class RevShellSession(BaseRevShellProtocol):
     conn_counter = itertools.count()
 
     def __init__(self, loop: AbstractEventLoop, manager):
-        self.loop = loop
+        self._loop = loop
         super().__init__(
-            stream_reader=StreamReader(loop=self.loop),
+            stream_reader=StreamReader(loop=self._loop),
             client_connected_cb=self._connection_made_cb,
-            loop=self.loop,
+            loop=self._loop,
         )
         self.manager = manager
-        self._reader: Optional[StreamReader] = None
-        self._writer: Optional[StreamWriter] = None
-        self.transport = None
+        self.conn: Optional[TCPStream] = None
+        self._transport = None
         self.connection_closed: bool = True
         self._connection_closed: bool = True
         self._conn_handler_task: Optional[Task] = None
         self._data_handler_task: Optional[Task] = None
         self._send_cmd_task: Optional[Task] = None
+        self._session_id: int = next(RevShellSession.conn_counter)
 
     def connection_made(self, transport):
+        self._transport = transport
         super().connection_made(transport)
-        self.transport = transport
-        self.manager.sessions.append(self)
         self.connection_closed = False
-        print(f'connection made from {transport.get_extra_info("peername")}')
-        self._send_cmd_task = self.loop.create_task(self._send_cmd())
+        self._send_cmd_task = self._loop.create_task(self.send_cmd())
 
     def data_received(self, data: bytes):
         super().data_received(data)
-        self._data_handler_task = self.loop.create_task(self._handle_data())
+        self._data_handler_task = self._loop.create_task(self._handle_data())
         if self._send_cmd_task.done():
-            self._send_cmd_task = self.loop.create_task(self._send_cmd())
+            self._send_cmd_task = self._loop.create_task(self.send_cmd())
 
-    def connection_lost(self, exc):
+    def connection_lost(self, exc: Exception):
         super().connection_lost(exc)
         self.connection_closed = self._connection_closed = True
         try:
@@ -55,15 +56,18 @@ class RevShellSession(BaseRevShellProtocol):
             pass
 
     async def _handle_data(self):
-        data = await self._reader.read(8192)
-        self.manager.stdout_writer.write(data)
-        await self.manager.stdout_writer.drain()
+        data = await self.conn.read()
+        await self.manager.stdio.write(data)
 
-    async def _send_cmd(self):
-        cmd = await self.manager.stdin_reader.readline()
-        self._writer.write(cmd)
-        await self._writer.drain()
+    async def send_cmd(self):
+        cmd = await self.manager.stdio.readline()
+        await self.conn.write(cmd)
 
     def _connection_made_cb(self, reader, writer):
-        self._reader = self.reader = reader
-        self._writer = self.writer = writer
+        self.conn = TCPStream.create(self._loop, reader, writer)
+        self.manager.sessions.append(self)
+        print(f'Connection from {self._transport.get_extra_info("peername")}', self._session_id)
+
+
+class LocalSession(SubprocessProtocol):
+    pass
